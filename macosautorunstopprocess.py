c896b1467ce2888328f4b567e2323026b68aec62
Roman <j@tjo.biz>
2020-09-27 05:01:03 +0800

    Get all rows with "ESTABLISHED" to list

--- macosautorunstopprocess.py
+++ macosautorunstopprocess.py
@@ -1,17 +1,26 @@
 import subprocess
+import re
 
 process = subprocess.Popen(['lsof', '-i'],
                            stdout=subprocess.PIPE,
                            universal_newlines=True)
 
+#This is array get all active process after load OS
+autorun = []
+
 while True:
     output = process.stdout.readline()
-    print(output.strip())
+    #print(output.strip())
     # Do something else
     return_code = process.poll()
     if return_code is not None:
         print('RETURN CODE', return_code)
         # Process has finished, read rest of the output
         for output in process.stdout.readlines():
-            print(output.strip())
+            row = re.search('ESTABLISHED', output)
+            if row is not None:
+                autorun.append(row.string)
+            #print(output.strip())
         break
+
+print(autorun)
